cc42c4135bc41758c623ea6f5ac3315a9fe4796a
Tristan Poveda <povedatristan@gmail.com>
2024-12-14 15:28:08 +0100

    Feat : Update UI

--- Assets/App/Scripts/Runtime/UI/CustomUIButton.cs
+++ Assets/App/Scripts/Runtime/UI/CustomUIButton.cs
@@ -1,17 +1,16 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
-public class CustomUIButton : MonoBehaviour, IPointerDownHandler
+public class CustomUIButton : MonoBehaviour, IPointerDownHandler, IPointerEnterHandler, IPointerExitHandler
 {
-    [Header("Settings")]
-    [SerializeField] private RectTransform swayObject;
-    [SerializeField] private CanvasGroup normal;
-    [SerializeField] private CanvasGroup pressed;
+    //[Header("Settings")]
+
+    [Header("References")]
     [SerializeField] private Animator animator;
-    [SerializeField] private AnimationClip clip;
-
-    //[Header("References")]
-
+    [SerializeField] private AnimationClip onButtonHoverClip;
+    [SerializeField] private AnimationClip onButtonUnHoverClip;
+    [SerializeField] private AnimationClip onButtonPressedClip;
     //[Space(10)]
     // RSO
     // RSF
@@ -21,14 +20,25 @@ public class CustomUIButton : MonoBehaviour, IPointerDownHandler
     [Header("Output")]
     [SerializeField] private UnityEvent onClick;
 
-    private void Awake()
+    public void OnPointerEnter(PointerEventData data)
     {
-        if(normal != null) normal.alpha = 1.0f;
-        if(pressed != null) pressed.alpha = 0.0f;
+        if (onButtonHoverClip != null) animator.Play(onButtonHoverClip.name);
+    }
+
+    public void OnPointerExit(PointerEventData data)
+    {
+        if (onButtonUnHoverClip != null) animator.Play(onButtonUnHoverClip.name);
     }
 
     public void OnPointerDown(PointerEventData data)
     {
+        if (onButtonPressedClip != null) animator.Play(onButtonPressedClip.name);
+        StartCoroutine("GenerateOnClickEvent");
+    }
+
+    private IEnumerator GenerateOnClickEvent()
+    {
+        yield return new WaitForSecondsRealtime(0.1f);
         onClick?.Invoke();
     }
 }
