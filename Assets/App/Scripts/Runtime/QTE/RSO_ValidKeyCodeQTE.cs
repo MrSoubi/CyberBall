3242c6fdc34a78f46e4f4a2ce031a2a789bfcf13
MartinsFlorian <f.martins2005.fm@gmail.com>
2024-12-14 18:35:12 +0100

    QTE System

---
+++ Assets/App/Scripts/Runtime/QTE/RSO_ValidKeyCodeQTE.cs
@@ -0,0 +1,4 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RSO_ValidKeyCodeQTE", menuName = "RSO/RSO_ValidKeyCodeQTE")]
+public class RSO_ValidKeyCodeQTE : BT.ScriptablesObject.RuntimeScriptableObject<KeyCode>{}
