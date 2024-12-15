11620db8d85ac5606be606c6f9f4461e822e782b
N3crom <mathias.manenq@gmail.com>
2024-12-15 11:48:53 +0100

    fix

---
+++ Assets/App/Scripts/Runtime/Player/Player.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+public class Player : MonoBehaviour
+{
+    //[Header("Settings")]
+
+    //[Header("References")]
+   
+
+
+    //[Space(10)]
+    // RSO
+    // RSF
+    // RSP
+
+    //[Header("Input")]
+    //[Header("Output")]
+}
