c4058c3cab2b8ea6159311a90597e7d4beb5f475
Steamcollect <eliot.garnier.080805@gmail.com>
2024-12-15 14:59:22 +0100

    Feat : player gender & id

---
+++ Assets/App/Scripts/Wrapper/RSE/RSE_SetupPlayerID.cs
@@ -0,0 +1,4 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RSE_SetupPlayerID", menuName = "RSE/RSE_SetupPlayerID")]
+public class RSE_SetupPlayerID : BT.ScriptablesObject.RuntimeScriptableEvent<string>{}
