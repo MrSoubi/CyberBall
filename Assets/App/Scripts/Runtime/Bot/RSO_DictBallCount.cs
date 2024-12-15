d3745ecc7b524e267d56935cb8445b5d1aa8d31c
Tristan Poveda <povedatristan@gmail.com>
2024-12-15 13:08:07 +0100

    Fix Double folder

--- Assets/App/Scripts/Runtime/Bot/RSO_DictBallCount.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "RSO_DictBallCount", menuName = "RSO/RSO_DictBallCount")]
-public class RSO_DictBallCount : ScriptableObject
-{
-    public Dictionary<string, int> Value = new Dictionary<string, int>();
-}
--- Assets/App/Scripts/Runtime/Player/RSO_PlayerBallReceiveCount.cs
+++
@@ -1,4 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "RSO_PlayerBallReceiveCount", menuName = "RSO/RSO_PlayerBallReceiveCount")]
-public class RSO_PlayerBallReceiveCount : BT.ScriptablesObject.RuntimeScriptableObject<int>{}
